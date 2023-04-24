277a9c6d0280deec9f316b674a43d6219dad7281
Uharal <95139320+Uharal@users.noreply.github.com>
2023-04-25 01:24:37 +0800

    h

--- chatting-client/src/main/java/cn/edu/sustech/cs209/chatting/client/Controller.java
+++ chatting-client/src/main/java/cn/edu/sustech/cs209/chatting/client/Controller.java
@@ -2,20 +2,26 @@ package cn.edu.sustech.cs209.chatting.client;
 
 import cn.edu.sustech.cs209.chatting.common.Message;
 import javafx.application.Platform;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.HBox;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.util.Callback;
 
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
 import java.net.URL;
-import java.util.Objects;
-import java.util.Optional;
-import java.util.ResourceBundle;
+import java.util.*;
 import java.util.concurrent.atomic.AtomicReference;
 
 public class Controller implements Initializable {
@@ -25,22 +31,42 @@ public class Controller implements Initializable {
 
     String username;
 
+    HashSet<String> loggedUsers = new HashSet<>();
+
+    /*
+    * 初始化方法，在应用程序启动时调用，用于创建登录对话框并让用户输入他们的用户名。
+    * 当用户输入了有效的用户名后，该方法设置了实例变量username并创建了一个MessageCellFactory来设置聊天内容ListView的单元格样式。
+    * */
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
         Dialog<String> dialog = new TextInputDialog();
-        dialog.setTitle("Login");
+        dialog.setTitle("Welcome");
         dialog.setHeaderText(null);
-        dialog.setContentText("Username:");
+        dialog.setContentText("请输入任意字符开始\n");
 
         Optional<String> input = dialog.showAndWait();
+
         if (input.isPresent() && !input.get().isEmpty()) {
             /*
                TODO: Check if there is a user with the same name among the currently logged-in users,
                      if so, ask the user to change the username
              */
-            username = input.get();
+
+            String name = username;
+            while (loggedUsers.contains(name)) {
+                dialog.setContentText("Username " + name + " is already taken. Please choose another username:");
+                input = dialog.showAndWait();
+                if (!input.isPresent() || input.get().isEmpty()) {
+                    System.out.println("Invalid username " + input + ", exiting");
+                    Platform.exit();
+                }
+                name = input.get();
+            }
+            username = name;
+            loggedUsers.add(username);
         } else {
+
             System.out.println("Invalid username " + input + ", exiting");
             Platform.exit();
         }
@@ -49,7 +75,61 @@ public class Controller implements Initializable {
     }
 
     @FXML
-    public void createPrivateChat() {
+    public void Continue(){
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("提示");
+        alert.setHeaderText(null);
+        alert.setContentText("未完待续\n" +
+                "Contact\n  " +
+                "QQ:2290477984");
+        alert.showAndWait();
+    }
+    @FXML
+    public void Readme() throws IOException {
+        Stage stage = new Stage();
+        HBox box = new HBox(10);
+        box.setAlignment(Pos.CENTER);
+        box.setPadding(new Insets(20, 20, 20, 20));
+        TextArea textArea = new TextArea();
+        textArea.setWrapText(true);
+        textArea.setEditable(false);
+        textArea.setText("这里是总控制台的使用说明。\n" +
+                "这是一个用javafx + socket实现的一个聊天室\n" +
+                "你可以通过点击页面上方导航栏的client打开客户端窗口\n" +
+                "通过点击页面上方导航栏的server打开服务器窗口\n" +
+                "你可以通过点击说明窗口左侧的导航栏进入具体的c-s的功能说明");
+
+//        content
+        Accordion accordion = new Accordion();
+
+        TitledPane pane1 = new TitledPane();
+        pane1.setText("Client");
+        Text text1 = new Text("You should open server first.\n" +
+                "");
+        pane1.setContent(text1);
+
+        TitledPane pane2 = new TitledPane();
+        pane2.setText("Server");
+        Text text2 = new Text("Explanation for Server");
+        pane2.setContent(text2);
+
+        TitledPane pane3 = new TitledPane();
+        pane3.setText("Other");
+        Text text3 = new Text("Continue.....");
+        pane3.setContent(text3);
+
+        accordion.getPanes().addAll(pane1, pane2,pane3);
+
+        box.setAlignment(Pos.CENTER);
+        box.setPadding(new Insets(20, 20, 20, 20));
+        box.getChildren().addAll(accordion, textArea);
+
+        stage.setScene(new Scene(box));
+        stage.showAndWait();
+
+    }
+    @FXML
+    public void createPrivateChat() throws IOException {
         AtomicReference<String> user = new AtomicReference<>();
 
         Stage stage = new Stage();
@@ -73,6 +153,17 @@ public class Controller implements Initializable {
 
         // TODO: if the current user already chatted with the selected user, just open the chat with that user
         // TODO: otherwise, create a new chat item in the left panel, the title should be the selected user's name
+        // 如果当前用户已经与所选用户聊过天，则打开该用户的聊天
+        for (Message message : chatContentList.getItems()) {
+            System.out.println("here");
+            if (message.getSentBy().equals(user.get())) {
+                // TODO: 打开该用户的聊天
+
+
+            }
+        }
+
+
     }
 
     /**
@@ -87,6 +178,19 @@ public class Controller implements Initializable {
      */
     @FXML
     public void createGroupChat() {
+        final JFrame frame = new JFrame();
+        JButton button = new JButton("打开");
+        frame.setBounds(300, 200, 500, 500);
+        frame.getContentPane().add(button);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+
+        button.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent arg0) {
+                JFrameShaker dialog1 = new JFrameShaker(frame);
+                dialog1.startShake();
+            }
+        });
     }
 
     /**
@@ -142,4 +246,5 @@ public class Controller implements Initializable {
             };
         }
     }
+
 }
